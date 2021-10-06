55a1d43ebeb295a11fd34a57d60b2d621874c03d
Maltann <69165401+Maltann@users.noreply.github.com>
2021-10-06 15:24:41 +0200

    Add files via upload

---
+++ passeword_generator.py
@@ -0,0 +1,15 @@
+import random
+
+chars_list = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!%ù$£&€^/*-(`)[]="
+other_password = "yes"
+
+while other_password == "yes":
+    password_length = int(input("What length do you want for your passeword ? "))
+    password_count = int(input("How many passeword do you want ? "))
+    for i in range(1,password_count+1):
+        password = ""
+        for j in range(password_length):
+            password_chars = random.choice(chars_list)
+            password = password + password_chars
+        print(f"Password {i} : {password}")
+    other_password = input("Do you want to generate other password(s) ? (yes or no) : ")
