1ad89bbb30c4fd2562ced445a1756afea8dc78cc
Maltann <69165401+Maltann@users.noreply.github.com>
2021-10-09 23:08:52 +0200

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -4,8 +4,8 @@ chars_list = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!%ù
 other_password = "yes"
 
 while other_password == "yes":
-    password_length = int(input("What length do you want for your passeword ? "))
-    password_count = int(input("How many passeword do you want ? "))
+    password_length = int(input("What length do you want for your password ? "))
+    password_count = int(input("How many password do you want ? "))
     for i in range(1,password_count+1):
         password = ""
         for j in range(password_length):
