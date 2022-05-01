9bdf7d85fcc421eba61bb1c55cc1c679af63f800
Maltann <69165401+Maltann@users.noreply.github.com>
2022-05-01 10:05:51 +0200

    Save option and code remake
    Making the code more clean using a function to generate the passwords and adding a save option to the script in a .txt file

--- password_generator.py
+++ password_generator.py
@@ -1,15 +1,27 @@
 import random
 
-chars_list = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!%ù$£&€^/*-(`)[]="
-other_password = "yes"
+chars_list = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?%$&€/*-()[]="
 
-while other_password == "yes":
-    password_length = int(input("What length do you want for your password ? "))
-    password_count = int(input("How many password do you want ? "))
-    for i in range(1,password_count+1):
-        password = ""
-        for j in range(password_length):
-            password_chars = random.choice(chars_list)
-            password = password + password_chars
-        print(f"Password {i} : {password}")
-    other_password = input("Do you want to generate other password(s) ? (yes or no) : ")
+def generate_password(length, chars):
+    password = ""
+    for i in range(length):
+        password_char = random.choice(chars)
+        password+=password_char
+    return password
+
+password_count = int(input("Number of password : "))
+password_length = int(input("Length of password : "))
+password_list = ""
+
+for j in range(password_count):
+    password = generate_password(password_length, chars_list)
+    print(f"password {j+1} : {password}")
+    password_list += password + "\n"
+
+save_password = input("Do you want to save the passwords ? Y/N : ")
+
+if save_password.lower() == "y":
+    with open("password_log.txt", "a") as psw:
+        psw.write("---New round---\n")
+        psw.write(password_list)
+    print("passwords has been succesfully saved !")
